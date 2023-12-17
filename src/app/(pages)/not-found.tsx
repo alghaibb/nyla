2281dbc79234ae08564154ae751a0d756aebe4f4
Moudi <mjaderi97@gmail.com>
2023-12-17 16:41:08 +1100

    Add hover state to go home btn on not found 404 page

--- src/app/(pages)/not-found.tsx
+++ src/app/(pages)/not-found.tsx
@@ -2,13 +2,15 @@ import { Button } from '../_components/Button'
 import { Gutter } from '../_components/Gutter'
 import { VerticalPadding } from '../_components/VerticalPadding'
 
+import classes from './index.module.scss'
+
 export default function NotFound() {
   return (
     <Gutter>
       <VerticalPadding top="none" bottom="large">
         <h1 style={{ marginBottom: 0 }}>404</h1>
         <p>This page could not be found.</p>
-        <Button href="/" label="Go Home" appearance="primary" />
+        <Button href="/" label="Go Home" appearance="primary" className={classes.notFoundBtn} />
       </VerticalPadding>
     </Gutter>
   )
