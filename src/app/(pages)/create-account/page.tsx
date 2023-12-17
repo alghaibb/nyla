de29feaade8a08ee327a6c230aeb65210c2ed68a
Moudi <mjaderi97@gmail.com>
2023-12-17 16:29:23 +1100

    Remove unused code and update imports

--- src/app/(pages)/create-account/page.tsx
+++ src/app/(pages)/create-account/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Metadata } from 'next'
 import Image from 'next/image'
-import Link from 'next/link'
 
 import { RenderParams } from '../../_components/RenderParams'
 import { getMeUser } from '../../_utilities/getMeUser'
